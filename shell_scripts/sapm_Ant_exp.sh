b7c4c9be33c700e62028a59225e12b31faeb723b
root <root@csi-cluster-gpu14.dakao.io>
2019-02-20 11:06:08 +0900

    shell scripts were modified

--- shell_scripts/sapm_Ant_exp.sh
+++ shell_scripts/sapm_Ant_exp.sh
@@ -5,12 +5,29 @@
 #	     2(Grad + Resampling), 3(SAPM)
 #############################################
 
-#export CUDA_VISIBLE_DEVICES=0
-python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm0.5 --act_size 128 --min_d 0.3 --scale 0.5 --N_layers 2 --DPP 1 --seed 0 10 20 30 40 &
+export CUDA_VISIBLE_DEVICES=0
+python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm_5e-1_128 --act_size 128 --min_d 0.3 --scale 0.5 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
 
-#export CUDA_VISIBLE_DEVICES=0
-python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm0.2 --act_size 128 --min_d 0.3 --scale 0.2 --N_layers 2 --DPP 1 --seed 0 10 20 30 40 &
+export CUDA_VISIBLE_DEVICES=1
+python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm_1e-1_128 --act_size 128 --min_d 0.3 --scale 0.1 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
  
-#export CUDA_VISIBLE_DEVICES=0
-python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm0.1 --act_size 128 --min_d 0.3 --scale 0.1 --N_layers 2 --DPP 1 --seed 0 10 20 30 40 &
+export CUDA_VISIBLE_DEVICES=2
+python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm_1e-2_128 --act_size 128 --min_d 0.3 --scale 0.01 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 
+
+export CUDA_VISIBLE_DEVICES=3
+python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm_5e-1_64 --act_size 64 --min_d 0.3 --scale 0.5 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
+
+export CUDA_VISIBLE_DEVICES=0
+python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm_1e-1_64 --act_size 64 --min_d 0.3 --scale 0.1 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
  
+export CUDA_VISIBLE_DEVICES=1
+python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm_1e-2_64 --act_size 64 --min_d 0.3 --scale 0.01 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 
+
+export CUDA_VISIBLE_DEVICES=2
+python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm_5e-1_16 --act_size 16 --min_d 0.3 --scale 0.5 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
+
+export CUDA_VISIBLE_DEVICES=3
+python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm_1e-1_16 --act_size 16 --min_d 0.3 --scale 0.1 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
+ 
+export CUDA_VISIBLE_DEVICES=0
+python -m spinup.run sapm --env Ant-v2 --exp_name Ant-sapm_1e-2_16 --act_size 16 --min_d 0.3 --scale 0.01 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 
--- shell_scripts/sapm_HalfCheetah_exp.sh
+++ shell_scripts/sapm_HalfCheetah_exp.sh
@@ -5,12 +5,29 @@
 #	     2(Grad + Resampling), 3(SAPM)
 #############################################
 
-#export CUDA_VISIBLE_DEVICES=0
-python -m spinup.run sapm --env Swimmer-v2 --exp_name Swimmer-sapm0.5 --act_size 128 --min_d 0.3 --scale 0.5 --N_layers 2 --DPP 1 --seed 0 10 20 30 40
+export CUDA_VISIBLE_DEVICES=0
+python -m spinup.run sapm --env HalfCheetah-v2 --exp_name Half-sapm_5e-1_128 --act_size 128 --min_d 0.3 --scale 0.5 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
 
-#export CUDA_VISIBLE_DEVICES=0
-python -m spinup.run sapm --env Swimmer-v2 --exp_name Swimmer-sapm0.2 --act_size 128 --min_d 0.3 --scale 0.2 --N_layers 2 --DPP 1 --seed 0 10 20 30 40 &
+export CUDA_VISIBLE_DEVICES=1
+python -m spinup.run sapm --env HalfCheetah-v2 --exp_name Half-sapm_1e-1_128 --act_size 128 --min_d 0.3 --scale 0.1 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
  
-#export CUDA_VISIBLE_DEVICES=0
-python -m spinup.run sapm --env Swimmer-v2 --exp_name Swimmer-sapm0.1 --act_size 32 --min_d 0.1 --scale 0.1 --N_layers 2 --DPP 1 --seed 0 10 20 30 40 &
+export CUDA_VISIBLE_DEVICES=2
+python -m spinup.run sapm --env HalfCheetah-v2 --exp_name Half-sapm_1e-2_128 --act_size 128 --min_d 0.3 --scale 0.01 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 
+
+export CUDA_VISIBLE_DEVICES=3
+python -m spinup.run sapm --env HalfCheetah-v2 --exp_name Half-sapm_5e-1_64 --act_size 64 --min_d 0.3 --scale 0.5 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
+
+export CUDA_VISIBLE_DEVICES=0
+python -m spinup.run sapm --env HalfCheetah-v2 --exp_name Half-sapm_1e-1_64 --act_size 64 --min_d 0.3 --scale 0.1 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
  
+export CUDA_VISIBLE_DEVICES=1
+python -m spinup.run sapm --env HalfCheetah-v2 --exp_name Half-sapm_1e-2_64 --act_size 64 --min_d 0.3 --scale 0.01 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 
+
+export CUDA_VISIBLE_DEVICES=2
+python -m spinup.run sapm --env HalfCheetah-v2 --exp_name Half-sapm_5e-1_16 --act_size 16 --min_d 0.3 --scale 0.5 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
+
+export CUDA_VISIBLE_DEVICES=3
+python -m spinup.run sapm --env HalfCheetah-v2 --exp_name Half-sapm_1e-1_16 --act_size 16 --min_d 0.3 --scale 0.1 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 &
+ 
+export CUDA_VISIBLE_DEVICES=0
+python -m spinup.run sapm --env HalfCheetah-v2 --exp_name Half-sapm_1e-2_16 --act_size 16 --min_d 0.3 --scale 0.01 --N_layers 3 --DPP 0 --seed 0 10 20 30 40 
